4f055c9c6dd38dea558779edc9cbf0cf6cfffebf
root <rounakdatta12@gmail.com>
2019-10-01 07:37:03 +0000

    env file setup

--- simple.go
+++ simple.go
@@ -6,12 +6,15 @@ import (
 	"os"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
 	fmt.Println("Connecting to database")
 
-	databaseCredentials := fmt.Sprintf("%s:%s/%s", os.Getenv("USER"), os.Getenv("PASSWORD"), os.Getenv("DATABASE"))
+	databaseCredentials := fmt.Sprintf("%s:%s@/%s", os.Getenv("APP_USER"), os.Getenv("APP_PASSWORD"), os.Getenv("APP_DATABASE"))
+	fmt.Println(databaseCredentials)
 	db, err := sql.Open("mysql", databaseCredentials)
 
 	if err != nil {
